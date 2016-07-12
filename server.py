bcde78885f514279496e17734fbcf0c6326f18e1
Anthony Minnella <anthony@Anthonys-MacBook-Pro-2.local>
2016-07-11 23:09:13 -0700

    conference

--- server.py
+++ server.py
@@ -99,7 +99,7 @@ def join():
                            from_="+15204403178")
     print(call.sid)
     
-    resp = "<Response><Dial><Conference>" + conf_name + "</Conference></Dial></Response>"
+    resp = "<Response><Dial><Conference startConferenceOnEnter=\"true\" endConferenceOnExit=\"true\">" + conf_name + "</Conference></Dial></Response>"
     return str(resp)
 
 @app.route('/', methods=['GET', 'POST'])
