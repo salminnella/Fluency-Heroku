73f96791d3c6da5b2ced1ab30440ba595cbc0e4c
Anthony Minnella <anthony@Anthonys-MacBook-Pro-2.local>
2016-08-02 22:58:52 -0700

    recordings callback

--- server.py
+++ server.py
@@ -118,7 +118,7 @@ def recordings():
     #recordings = twilioClient.recordings.list(CallSid=call.sid)
     recording = twilioClient.recordings.list("CAcef57878b66811f153430b911de75003")
     #CallSid = "CAd3e777bd7c010db188fb0c8d722339eb"
-    print(recording.url)
+    print(recording)
 #return recording
 
 @app.route('/', methods=['GET', 'POST'])
