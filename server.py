c831ff3b8fd1943ecb4c7758770d602a055b037f
Anthony Minnella <anthony@Anthonys-MacBook-Pro-2.local>
2016-08-12 02:34:27 -0700

    adding language and country code

--- server.py
+++ server.py
@@ -156,7 +156,7 @@ def pushCallHistory():
 
     {u'name': u'-Io26123nDHkfybDIGl7'}
 
-    return str(<Response>)
+    return '<Response'
 
 @app.route('/pushRecordedCallHistory', methods=['GET', 'POST'])
 def pushRecordedCallHistory():
