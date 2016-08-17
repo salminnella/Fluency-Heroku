c23135955259d0a15f35ccc99a4b91abe3e45757
Anthony Minnella <anthony@Anthonys-MacBook-Pro-2.local>
2016-08-16 23:59:29 -0700

    conference call duration

--- server.py
+++ server.py
@@ -193,7 +193,7 @@ def pushConfHistory():
     conference = client.conferences.get("CFc559d38e52807e0aaa1e81b9c4217dbb")
     
     #return str(conferenceCallSid)
-    return str(conference.datecreated)
+    return str(conference.date_created)
 
 
 @app.route('/pushRecordedConfHistory', methods=['GET', 'POST'])
