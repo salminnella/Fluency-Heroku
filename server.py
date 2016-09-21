2b62f5f16feac31b0df0aac0036c42f506fb8f87
Anthony Minnella <anthony@Anthonys-MacBook-Pro-2.local>
2016-09-21 09:30:19 -0700

    error responses-stripe

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import os
 import stripe
+from stripe.error import (  # noqa
+                          StripeError, APIError, APIConnectionError, AuthenticationError, CardError,
+                          InvalidRequestError)
 from flask import Flask, request
 from twilio.util import TwilioCapability
 from twilio.rest import TwilioRestClient
