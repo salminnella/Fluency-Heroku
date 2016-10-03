c394f80fdd7db4b123c8a0106e52b051ef625206
Anthony Minnella <anthony@Anthonys-MacBook-Pro-2.local>
2016-10-03 09:05:17 -0700

    testing parameters to callback method

--- server.py
+++ server.py
@@ -82,7 +82,7 @@ def call():
   userId = request.values.get('userID')
 
 #  params = request.query_string
-  params = "userID=" + userId + "&nextCallHistoryId=" + new_callHistoryID + "&countryCode=" + urlencode(countryCode) + "&interpreterLanguage=" + interLanguage + "&sourceLanguage=" + srcLanguage + "&CallDateTime=" + urllib.quote_plus(callDateTime) + "&number=" + number + "&name=" + name + "&callType=" + urllib.quote(callType)
+  params = "userID=" + userId + "&nextCallHistoryId=" + new_callHistoryID + "&countryCode=" + urllib.quote_plus(countryCode) + "&interpreterLanguage=" + interLanguage + "&sourceLanguage=" + srcLanguage + "&CallDateTime=" + urllib.quote_plus(callDateTime) + "&number=" + number + "&name=" + name + "&callType=" + urllib.quote(callType)
   resp = twilio.twiml.Response()
   from_value = request.values.get('From')
   conf_name = request.values.get('ConfName')
