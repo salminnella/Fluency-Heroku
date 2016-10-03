2249f84ae59047166298cde3ba2407a615309e90
Anthony Minnella <anthony@Anthonys-MacBook-Pro-2.local>
2016-10-03 14:03:47 -0700

    testing parameters to callback method

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ from urllib import urlencode
 # Account Sid and Auth Token can be found in your account dashboard
 ACCOUNT_SID = 'ACdd8953205cab360450e486f1a3a52fe9'
 AUTH_TOKEN = '4eea9c2481e3f5f8b630a7d30942a1b6'
-    
+
 global firebase
 firebase = firebase.FirebaseApplication('https://project-5176964787746948725.firebaseio.com')
 
@@ -93,7 +93,7 @@ def call():
   recordCall = request.values.get('RecordCall')
   caller_id = os.environ.get("CALLER_ID", CALLER_ID)
   digits = request.values.get('SendDigits')
-  
+
 
   if digits:
       output = "<Response><Dial callerId=\"" + caller_id + "\"><Number sendDigits=\"wwwwww4860\">" + to + "</Number></Dial></Response>"
@@ -134,9 +134,9 @@ def call():
 
 @app.route('/conference', methods=['GET', 'POST'])
 def conference():
-    
+
     conf_name = request.values.get('ConfName')
-    
+
     if conf_name:
         resp = "<Response><Dial><Conference>" + conf_name + "</Conference></Dial></Response>"
         return resp
@@ -152,26 +152,31 @@ def join():
                            to = request.values.get('To'),
                            from_="+15204403178"
                            )
-    
+
     resp = "<Response><Dial><Conference>" + conf_name + "</Conference></Dial></Response>"
     return str(resp)
 
 @app.route('/pushCallHistory', methods=['GET', 'POST'])
 def pushCallHistory():
-    
+    params = request.query_string
+    print "params = " + params
+    d = dict(item.split("=") for item in params.split("%26"))
+
     #one call to interpreter - Face to face
     callSid = request.values.get('DialCallSid')
     callDuration = request.values.get('DialCallDuration')
-    userID = request.values.get('userID')
-      
-    callType = request.values.get('callType')
-    name = request.values.get('name')
-    number = request.values.get('number')
-    callDateTime = request.values.get('CallDateTime')
-    srcLanguage = request.values.get('sourceLanguage')
-    interLanguage = request.values.get('interpreterLanguage')
-    countryCode = request.values.get('countryCode')
-    new_callHistoryID = request.values.get('nextCallHistoryId')
+
+    userID = d['userID']
+    callTypeEncoded = d['callType']
+    callType = callTypeEncoded.replace("%20", " ")
+    name = d['name']
+    number = d['number']
+    callDateTimeEncoded = d['CallDateTime']
+    callDateTime = callDateTimeEncoded.replace("%2F", "/")
+    srcLanguage = d['sourceLanguage']
+    interLanguage = d['interpreterLanguage']
+    countryCode = d['countryCode']
+    new_callHistoryID = d['nextCallHistoryId']
 
     #Ozgur - firebase push -- working
     result = firebase.put('/User/' + str(userID) + '/callHistory', str(new_callHistoryID), data={'callHistoryId': str(new_callHistoryID), 'callType': str(callType), 'callDuration': str(callDuration), 'callSID': str(callSid), 'callDateTime': str(callDateTime), 'number': str(number), 'name': str(name), 'srcLanguage': str(srcLanguage), 'interLanguage': str(interLanguage), 'countryCode': str(countryCode)})
@@ -182,61 +187,61 @@ def pushCallHistory():
 
 @app.route('/pushRecordedCallHistory', methods=['GET', 'POST'])
 def pushRecordedCallHistory():
-    
+
     #one call to interpreter - recorded - Face to face
     callSid = request.values.get('DialCallSid')
     callDuration = request.values.get('DialCallDuration')
     recordingUrl = request.values.get('RecordingUrl')
-    
+
     #Ozgur - firebase push -- working
     result = firebase.put('/User/' + userID + '/callHistory', new_callHistoryID, data={'callHistoryId': new_callHistoryID, 'callType': callType, 'callDuration': callDuration, 'callSID': callSid, 'callDateTime': callDateTime, 'number': number, 'name': name, 'recordingURI': recordingUrl, 'srcLanguage': srcLanguage, 'interLanguage': interLanguage, 'countryCode': countryCode})
-    
+
     {u'name': u'-Io26123nDHkfybDIGl7'}
-    
+
     return '<Response></Response>'
 
 @app.route('/pushConfHistory', methods=['GET', 'POST'])
 def pushConfHistory():
-    
+
     #conference info
     conferenceSid = request.values.get('ConferenceSid')
     conferenceCallSid = request.values.get('CallSid')
-    
+
     client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
     conference = client.conferences.get(conferenceSid)
     timestamp_created = mktime_tz(parsedate_tz(conference.date_created))
     timestamp_updated = mktime_tz(parsedate_tz(conference.date_updated))
-    
+
     duration = str(timestamp_updated - timestamp_created)
-    
+
     #Ozgur - firebase push -- working
     result = firebase.put('/User/' + userID + '/callHistory', new_callHistoryID, data={'callHistoryId': new_callHistoryID, 'callType': callType, 'callDuration': duration, 'conferenceSID': conferenceSid, 'callSID': conferenceCallSid, 'callDateTime': callDateTime, 'number': number, 'name': name, 'srcLanguage': srcLanguage, 'interLanguage': interLanguage, 'countryCode': countryCode, 'contactImage': contactImage})
 
     {u'name': u'-Io26123nDHkfybDIGl7'}
-    
+
     return str(conferenceCallSid)
 
 
 @app.route('/pushRecordedConfHistory', methods=['GET', 'POST'])
 def pushRecordedConfHistory():
-    
+
     #conference info - recorded
     conferenceSid = request.values.get('ConferenceSid')
     conferenceCallSid = request.values.get('CallSid')
     recordingUrl = request.values.get('RecordingUrl')
     duration = request.values.get('Duration')
     recordingTimestamp = request.values.get('timestamp')
-    
+
     #Ozgur - firebase push -- working
     result = firebase.put('/User/' + userID + '/callHistory', new_callHistoryID, data={'callHistoryId': new_callHistoryID, 'callType': callType, 'callDuration': duration, 'conferenceSID': conferenceSid, 'callSID': conferenceCallSid,'callDateTime': callDateTime,  'recordingURI': recordingUrl, 'number': number, 'name': name, 'srcLanguage': srcLanguage, 'interLanguage': interLanguage, 'countryCode': countryCode, 'contactImage': contactImage})
 
     {u'name': u'-Io26123nDHkfybDIGl7'}
-    
+
     return str(new_callHistoryID)
 
 @app.route('/delete-recording', methods=['GET', 'POST'])
 def recording():
-    
+
     recordingSID = request.values.get('RecordingSID')
     client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
     client.recordings.delete(recordingSID)
@@ -275,7 +280,7 @@ def chargeCustomer():
     cents = int(cost)
 
     response = chargeCard(custID, cents, emailAddress)
-    
+
     return response
 
 @app.route('/charge', methods=['GET', 'POST'])
@@ -416,7 +421,7 @@ def authCreditCard():
         jsonArray = json.dumps(err)
         preAuthResponse = jsonArray
         pass
-                                    
+
     return str(preAuthResponse)
 
 
@@ -431,7 +436,7 @@ def cancel_preauth():
 
 @app.route('/update_customer', methods=['GET', 'POST'])
 def update_customer():
-    
+
     stripeToken = request.values.get('stripeToken')
     custID = request.values.get('customerID')
 
@@ -452,7 +457,7 @@ def chargeCard( customerID, chargeAmount, emailAddress ):
                                         description="Charge for " + emailAddress
                                         )
         chargeResponse = "{ \"charge\": \"" + b_charge.id + "\"}"
-        
+
         pass
     except stripe.CardError as e:
         # Since it's a decline, stripe.error.CardError will be caught
