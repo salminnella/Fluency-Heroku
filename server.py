b43f34c57e4b64e498fcddd8d29716bd1f63fc8b
salminnella <salminnella@gmail.com>
2016-12-20 00:19:36 -0800

    logging conference end for firebase push

--- server.py
+++ server.py
@@ -200,7 +200,7 @@ def pushCallHistory():
 def pushRecordedCallHistory():
     encodedParams = request.query_string
     params = encodedParams.replace("%3D", "=")
-    print 'Params = ', str(params)
+    # print 'Params = ', str(params)
     d = dict(item.split("=") for item in params.split("%26"))
 
     #one call to interpreter - recorded - Face to face
@@ -286,6 +286,7 @@ def pushConfHistory():
         {u'name': u'-Io26123nDHkfybDIGl7'}
     elif callStatus == 'conference-end':
         #Ozgur - firebase push when call is completed -- working
+        print 'conference end was called'
         result = firebase.put('/User/' + userID + '/callHistory', new_callHistoryID, data={'callHistoryId': new_callHistoryID, 'callType': callType, 'callDuration': duration, 'conferenceSID': conferenceSid, 'callSID': conferenceCallSid, 'callDateTime': callDateTime, 'number': number, 'name': name, 'srcLanguage': srcLanguage, 'srcLanguageIso': srcLanguageIso, 'interLanguage': interLanguage, 'interLanguageIso': interLanguageIso, 'countryCode': countryCode})
         result = firebase.put('/User/' + userID + '/callLeft', {'sid': 'none'} )
         result = firebase.put('/User/' + userID + '/callJoin', {'sid': 'none'} )
